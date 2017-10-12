6573dad3e719f93569c6da698607a799ac0195dc
tsulej <tomeksul@gmail.com>
2017-10-12 14:01:53 +0200

    fixed a fix fuck

--- tomato.py
+++ tomato.py
@@ -61,7 +61,6 @@ with open(filein,'rb') as rd:
 				rd.seek(0)
 				wr.write(rd.read(pos)) # start the read at the beginning again,
 				wr.write(split[0])  # spit out data up to this point plus the stuff before idx
-                                print(len(split[0]))
                                 rd.seek(len(split[0])+8,1)
 				idx = split[1] + rd.read()
 				break
